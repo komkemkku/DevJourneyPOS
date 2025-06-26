a449bc048885c69cdd104df45af86d52f8441a27
komkem <komkem.k@kkumail.com>
2025-06-27 02:48:32 +0700

    .

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "http://devjourneypos-production.up.railway.app/api";
 let accessToken = localStorage.getItem("token") || "";
 
 let products = [];
